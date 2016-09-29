b6546aa96e9a1844ea25309e5fbce60cfd1fb379
biezhihua <biezhihua@163.com>
2016-09-29 14:56:13 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# PlayVideo-OpenGL
+
+通过Opengl ES去绘制视频的demo
---
+++ app/src/main/java/com/example/jarry/NativeMediaActivity.java
@@ -0,0 +1,111 @@
+package com.example.jarry;
+
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.opengl.GLSurfaceView;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Surface;
+
+import java.io.IOException;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+public class NativeMediaActivity extends AppCompatActivity implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener{
+    public static final String videoPath = Environment.getExternalStorageDirectory().getPath()+"/one.mp4";
+
+    private SurfaceTexture videoTexture;
+    private GLSurfaceView glView;
+    private IjkMediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        NativeMediaWrapper.nativeOnCreate();
+
+        glView = new GLSurfaceView(this);
+        setContentView(glView);
+        glView.setEGLContextClientVersion(2);
+        glView.setRenderer(this);
+
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        NativeMediaWrapper.nativeSurfaceCreated();
+        videoTexture = NativeMediaWrapper.nativeGetSurfaceTexture();
+        if (videoTexture != null) {
+            videoTexture.setOnFrameAvailableListener(this);
+        }
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        NativeMediaWrapper.nativeSurfaceChanged(width, height);
+
+        playVideo();
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        NativeMediaWrapper.nativeDrawFrame();
+    }
+
+
+    @Override
+    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+        NativeMediaWrapper.nativeFrameAailable();
+    }
+
+
+    private void playVideo() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new IjkMediaPlayer();
+            mediaPlayer.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(IMediaPlayer iMediaPlayer) {
+                    iMediaPlayer.start();
+                }
+            });
+            if (videoTexture == null) {
+                return;
+            }
+            Surface surface = new Surface(videoTexture);
+            mediaPlayer.setSurface(surface);
+            surface.release();
+            try {
+                mediaPlayer.setDataSource(videoPath);
+                mediaPlayer.prepareAsync();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+       // NativeMediaWrapper.nativeOnDestroy();
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+}
